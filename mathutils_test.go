7009c775efdec9887fe6a584323ee5e69d609a0d
jlafayette <joshlafayette@gmail.com>
2018-05-06 19:48:53 -0700

    Use table tests for Min and Max

--- mathutils_test.go
+++ mathutils_test.go
@@ -5,29 +5,38 @@ import (
 )
 
 func TestMax(t *testing.T) {
-	test := func(expected, x, y int) {
-		actual := Max(x, y)
-		if expected != actual {
-			t.Errorf("Test failed. Expected %d got %d", expected, actual)
-		}
+	tests := []struct {name string; expected int; x int; y int}{
+		{"xmax", 3, 3, 2},
+		{"same", 12, 12, 12},
+		{"xneg", 0, -2, 0},
+		{"2neg", -4, -4, -5},
+		{"zeros", 0, 0, 0},
 	}
-	test(3, 3, 2)
-	test(12, 12, 12)
-	test(0, -2, 0)
-	test(-4, -4, -5)
-	test(0, 0, 0)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := Max(test.x, test.y)
+			if test.expected != actual {
+				t.Errorf("got %d, want %d", test.expected, actual)
+			}
+		})
+	}
 }
 
+
 func TestMin(t *testing.T) {
-	test := func(expected, x, y int) {
-		actual := Min(x, y)
-		if expected != actual {
-			t.Errorf("Test failed. Expected %d got %d", expected, actual)
-		}
+	tests := []struct {name string; expected int; x int; y int}{
+		{"ymin", 2, 3, 2},
+		{"same", 12, 12, 12},
+		{"xneg", -2, -2, 0},
+		{"yneg", -5, -4, -5},
+		{"zeros", 0, 0, 0},
 	}
-	test(2, 3, 2)
-	test(12, 12, 12)
-	test(-2, -2, 0)
-	test(-5, -4, -5)
-	test(0, 0, 0)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := Min(test.x, test.y)
+			if test.expected != actual {
+				t.Errorf("got %d, want %d", test.expected, actual)
+			}
+		})
+	}
 }
